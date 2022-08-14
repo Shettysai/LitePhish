7222fc4353a1de1c875f52acd3979a375d6f8759
Saikumar Shetty <73155077+Shettysai@users.noreply.github.com>
2022-08-14 21:55:59 +0530

    Update post.php

--- modules/post.php
+++ modules/post.php
@@ -17,5 +17,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
 }
 //if(isset($_POST['link'])) echo "<script>window.location.replace('".$_POST['link']."');</script>";
 //else 
-echo "<script>console.log($_POST, $url)</script>";
+echo "<script>window.location.assign('$url')</script>";
 ?>
